7cd0aefb3984b0a2ce13a87a97398f562589a01f
Ivana <ivana_pile@yahoo.com>
2020-04-27 13:59:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random() * 10) ;
+// Math.random() returns a number between 0 and 0.999..., 
+//which we multiply by 10 to give us a number between 0 and 9.999.... 
+// We then pass that number to Math.floor(), which returns an integer between 0 and 9
+
+const mod = ( 32 / 8 );
+const max = Math.max(1, 5, 20)

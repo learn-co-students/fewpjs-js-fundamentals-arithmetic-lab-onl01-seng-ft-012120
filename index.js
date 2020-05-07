14c219372d5b9479d5ab4774422e24195410fcad
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-07 16:24:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10000) + 1;
+const mod = 16%6;
+const max = Math.max(1, 2, 5, 10, 14, 20);
